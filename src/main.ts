4086f58815651a06c4b0cfa745856b13c0287de6
Fiujy <momo.kerrouche31@gmail.com>
2024-11-22 10:53:05 +0100

    feat: ajout des autorisations

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://task-manager-front-2pcv.onrender.com/',
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
+    origin: 'https://task-manager-front-2pcv.onrender.com', 
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, 
   });
   await app.listen(process.env.PORT ?? 3000);
 }
